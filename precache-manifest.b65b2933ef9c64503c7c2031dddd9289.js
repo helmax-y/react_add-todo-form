b67a997fcb29e3f8b9605db0ef9aa81580b9511d
helmax-y <max_rad@ukr.net>
2019-12-11 00:24:16 +0200

    Updates

---
+++ precache-manifest.b65b2933ef9c64503c7c2031dddd9289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c504624e0802330557777c816937562",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "6a1650f11352f1bd26c7",
+    "url": "/react_add-todo-form/static/css/main.0d37276b.chunk.css"
+  },
+  {
+    "revision": "2a032cb8a3c2e81f734b",
+    "url": "/react_add-todo-form/static/js/2.dbdfff11.chunk.js"
+  },
+  {
+    "revision": "6a1650f11352f1bd26c7",
+    "url": "/react_add-todo-form/static/js/main.813de859.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
